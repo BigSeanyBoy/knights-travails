3ac28a68cb4595a8575482c577bb75c0ba96a563
Sean Hammell <sean.m.hammell@gmail.com>
2022-02-25 12:41:09 -0600

    Add #knight_moves along with backtracking and printing

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 # Node
 class Node
-  attr_accessor :location, :children
+  attr_accessor :location, :parent, :children
 
   def initialize(location, parent)
     @location = location
@@ -18,20 +18,53 @@ class Knight
     [2, -1], [1, -2], [-1, -2], [-2, -1]
   ].freeze
 
+  attr_accessor :queue
+
   def initialize(start)
     @root = Node.new(start, nil)
-    @queue = []
+    @queue = [@root]
+    @path = []
   end
 
-  def possible_moves(node = @root)
+  def possible_moves(node)
     TRANSFORMS.each do |transform|
       move_x = node.location[0] + transform[0]
       move_y = node.location[1] + transform[1]
-      next if move_x.between?(0, 7) && move_y.between?(0, 7)
+      next unless move_x.between?(0, 7) && move_y.between?(0, 7)
 
       move_node = Node.new([move_x, move_y], node)
+      # puts "[#{move_x}, #{move_y}]"
       node.children.push(move_node)
       queue.push(move_node)
     end
   end
+
+  def path_of_travail(node)
+    until node.nil?
+      @path.unshift("#{node.location} ")
+      node = node.parent
+    end
+  end
+
+  def print_path
+    move_count = @path.length - 1
+    move_plural = move_count == 1 ? 'move' : 'moves'
+    puts "\nYou made it in #{move_count} #{move_plural}! Here's your path:"
+    puts @path << "\n"
+  end
 end
+
+def knight_moves(start, finish)
+  knight = Knight.new(start)
+  node = knight.queue.shift
+  until node.location == finish
+    knight.possible_moves(node)
+    node = knight.queue.shift
+  end
+  knight.path_of_travail(node)
+  knight.print_path
+end
+
+knight_moves([0, 0], [1, 2])
+knight_moves([3, 3], [4, 3])
+knight_moves([7, 0], [0, 7])
